899a2d0871d315d817e62158233a73952abec2fc
nebutty <nebathussen2@gmail.com>
2025-08-26 13:26:30 +0300

    updated setting side_bar and setting page

--- news_brief/src/components/siderbar/setting.tsx
+++ news_brief/src/components/siderbar/setting.tsx
@@ -1,18 +1,26 @@
-// components/Sidebar.js
 "use client";
 import { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
 
 const Sidebar = () => {
-  const [activeItem, setActiveItem] = useState("Home");
+  const [activeItem, setActiveItem] = useState("Language & Translation");
 
-  const handleItemClick = (itemName: string) => {
+  const handleScroll = (id: string, itemName: string) => {
+    document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
     setActiveItem(itemName);
   };
 
+  const items = [
+    { id: "home", label: "Home" },
+    { id: "language", label: "Language & Translation" },
+    { id: "voice", label: "Voice Settings" },
+    { id: "content", label: "Content Preferences" },
+    { id: "accessibility", label: "Accessibility" },
+    { id: "general", label: "General" },
+    { id: "account", label: "Account" },
+  ];
+
   return (
-    <div className="w-64 bg-white text-black min-h-screen flex flex-col p-2 rounded-lg shadow-md">
+    <div className=" fixed w-64 bg-white text-black min-h-screen flex flex-col p-2 rounded-lg shadow-md">
       {/* Header with News Brief */}
       <div className=" m-4">
         <h1 className="text-2xl md:text-3xl font-bold m-2">Setting</h1>
@@ -21,31 +29,22 @@ const Sidebar = () => {
         </div>
       </div>
 
-      {/* Navigation Menu */}
-      <nav className="flex-1 p-4">
+      {/* Menu */}
+      <nav className="flex-1 p-4 overflow-y-auto">
         <ul className="space-y-2">
-          {[
-            "Home",
-            "Language & Translation",
-            "Voice Settings",
-            "Content Preferences",
-            "Accessibility",
-            "General",
-            "Account",
-          ].map((item) => (
-            <li key={item}>
-              <Link
-                // href={`/${item.toLowerCase().replace(" ", "-")}`}
-                href={`/`}
-                className={`flex items-center px-4 py-3 rounded-md transition-colors ${
-                  activeItem === item
-                    ? "bg-black text-white"
-                    : "text-black hover:[#dddfe2]"
-                }`}
-                onClick={() => handleItemClick(item)}
+          {items.map((item) => (
+            <li key={item.id}>
+              <button
+                onClick={() => handleScroll(item.id, item.label)}
+            className={`flex items-center w-full px-3 py-2 rounded-md transition-colors text-left text-sm ${
+              activeItem === item.label
+                ? "bg-black text-white"
+                : "text-black hover:bg-gray-100"
+            }`}
               >
-                <span className="mr-3 w-5 h-5 flex items-center justify-center">
-                  {item === "Home" && (
+                <span className="mr-2 w-4 h-4 flex items-center justify-center">
+                  {/* icons based on label */}
+                  {item.label === "Home" && (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="20"
@@ -56,36 +55,7 @@ const Sidebar = () => {
                       <path d="M4 21V9l8-6l8 6v12h-6v-7h-4v7z" />
                     </svg>
                   )}
-                  {item === "Language & Translation" && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        fill="#000"
-                        d="m12.87 15.07l-2.54-2.51l.03-.03A17.5 17.5 0 0 0 14.07 6H17V4h-7V2H8v2H1v2h11.17C11.5 7.92 10.44 9.75 9 11.35C8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5l3.11 3.11zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2zm-2.62 7l1.62-4.33L19.12 17z"
-                      />
-                    </svg>
-                  )}
-                  {item === "Voice Settings" && (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      viewBox="0 0 24 24"
-                    >
-                      <g fill="none">
-                        <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-                        <path
-                          fill="#000"
-                          d="M13.26 3.3a1.1 1.1 0 0 1 1.734.78l.006.114v15.612a1.1 1.1 0 0 1-1.643.957l-.096-.062L6.68 16H4a2 2 0 0 1-1.995-1.85L2 14v-4a2 2 0 0 1 1.85-1.995L4 8h2.68zm6.407 3.483A7 7 0 0 1 22 12a7 7 0 0 1-2.333 5.217a1 1 0 1 1-1.334-1.49A5 5 0 0 0 20 12c0-1.48-.642-2.81-1.667-3.727a1 1 0 1 1 1.334-1.49m-2 2.236A4 4 0 0 1 19 11.999a4 4 0 0 1-1.333 2.982a1 1 0 0 1-1.422-1.4l.088-.09c.41-.368.667-.899.667-1.491a2 2 0 0 0-.548-1.376l-.119-.115a1 1 0 1 1 1.334-1.49"
-                        />
-                      </g>
-                    </svg>
-                  )}
-                  {item === "Content Preferences" && (
+                  {item.label === "Language & Translation" && (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="20"
@@ -93,10 +63,32 @@ const Sidebar = () => {
                       viewBox="0 0 24 24"
                       fill="currentColor"
                     >
-                      <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5 2 5.41 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.08C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.41 22 8.5c0 3.77-3.4 6.86-8.55 11.53z" />
+                      <path d="m12.87 15.07-2.54-2.51.03-.03A17.5 17.5 0 0014.07 6H17V4h-7V2H8v2H1v2h11.17C11.5 7.92 10.44 9.75 9 11.35A17 17 0 016.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56L2.58 17.58 4 19l5-5 3.11 3.11z" />
                     </svg>
                   )}
-                  {item === "Accessibility" && (
+                  {item.label === "Voice Settings" && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                    >
+                      <path d="M13.26 3.3a1.1 1.1 0 011.74.89v15.61a1.1 1.1 0 01-1.74.89L6.68 16H4a2 2 0 01-2-2v-4a2 2 0 012-2h2.68l6.58-4.7z" />
+                    </svg>
+                  )}
+                  {item.label === "Content Preferences" && (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                    >
+                      <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.27 2 8.5A5.5 5.5 0 017.5 3c1.74 0 3.41.81 4.5 2.08A5.5 5.5 0 0116.5 3 5.5 5.5 0 0122 8.5c0 3.77-3.4 6.86-8.55 11.53z" />
+                    </svg>
+                  )}
+                  {item.label === "Accessibility" && (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="20"
@@ -107,7 +99,7 @@ const Sidebar = () => {
                       <path d="M12 2c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm9 7h-6v13h-2v-6h-2v6H9V9H3V7h18v2z" />
                     </svg>
                   )}
-                  {item === "General" && (
+                  {item.label === "General" && (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="20"
@@ -118,7 +110,7 @@ const Sidebar = () => {
                       <path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58c.18-.14.23-.41.12-.61l-1.92-3.32c-.12-.22-.37-.29-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54c-.04-.24-.24-.41-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96c-.22-.08-.47 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.09.63-.09.94s.02.64.07.94l-2.03 1.58c-.18.14-.23.41-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6c-1.98 0-3.6-1.62-3.6-3.6s1.62-3.6 3.6-3.6 3.6 1.62 3.6 3.6-1.62 3.6-3.6 3.6z" />
                     </svg>
                   )}
-                  {item === "Account" && (
+                  {item.label === "Account" && (
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="20"
@@ -129,9 +121,10 @@ const Sidebar = () => {
                       <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
                     </svg>
                   )}
+                  {/* you can add icons for Accessibility, General, Account too */}
                 </span>
-                {item}
-              </Link>
+                {item.label}
+              </button>
             </li>
           ))}
         </ul>
