76a56e684015e196a9649928c5acdacd62aeb1b8
meklit <meklitmelkamu34@gmail.com>
2025-08-26 16:18:43 +0300

    Merge main into feature/frontend/interestsAndsignup/signin

--- news_brief/src/components/signin_components/siginCard.tsx
+++ news_brief/src/components/signin_components/siginCard.tsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Image from "next/image";
 
 const SignInCard = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <div
-        className="w-full max-w-md  rounded-2xl shadow-xl p-8"
+        className="w-full max-w-md rounded-2xl shadow-xl p-8"
         style={{ background: "rgb(245, 245, 245)" }}
       >
         <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">
           Welcome Back
         </h1>
+
         <div className="mb-4">
           <input
             type="email"
@@ -25,15 +27,16 @@ const SignInCard = () => {
             className="w-full px-4 py-3 rounded-xl border border-gray-200 bg-gray-50 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black transition"
           />
         </div>
-        <div className="mb-6">
-          <a href="#" className="hover:underline font-medium text-[12px] pl-[280px] ">
+
+        <div className="mb-6 text-right">
+          <a href="#" className="hover:underline font-medium text-[12px]">
             Forgot Password?
           </a>
         </div>
+
         <button className="w-full bg-black text-white py-3 rounded-[30px] font-semibold hover:bg-gray-900 transition">
           Sign In
         </button>
-       
 
         <div className="flex items-center my-6">
           <hr className="flex-1 border-gray-300" />
@@ -42,22 +45,27 @@ const SignInCard = () => {
         </div>
 
         <button
-          className="w-full border  py-3 rounded-[30px] flex items-center justify-center gap-3 text-black font-medium "
+          className="w-full border py-3 rounded-[30px] flex items-center justify-center gap-3 text-black font-medium"
           aria-label="Sign in with Google"
         >
-          <img src="/images/google.png" className="max-w-5" alt="" />
+          <Image
+            src="/images/google.png"
+            width={24}
+            height={24}
+            alt="Google Logo"
+          />
           Continue with Google
         </button>
+
         <div>
-            <p className="mt-6 text-center text-gray-500 text-sm">
-                Don't have an account?{" "}
-                <a href="#" className="hover:underline font-medium text-black">
-                Sign Up
-                </a>
-            </p>
+          <p className="mt-6 text-center text-gray-500 text-sm">
+            Don&apos;t have an account?{" "}
+            <a href="#" className="hover:underline font-medium text-black">
+              Sign Up
+            </a>
+          </p>
         </div>
       </div>
-
     </div>
   );
 };
