74eeec6b596f4edd161ecf9408c4722389b8e8f5
meklit <meklitmelkamu34@gmail.com>
2025-08-26 15:54:50 +0300

    feat(frontend): add all interest selection and signup/signin  components and pages into correct folder structure

---
+++ news_brief/src/components/signin_components/siginCard.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const SignInCard = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div
+        className="w-full max-w-md  rounded-2xl shadow-xl p-8"
+        style={{ background: "rgb(245, 245, 245)" }}
+      >
+        <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">
+          Welcome Back
+        </h1>
+        <div className="mb-4">
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full px-4 py-3 rounded-xl border border-gray-200 bg-gray-50 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black transition"
+          />
+        </div>
+
+        <div className="mb-0.5">
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full px-4 py-3 rounded-xl border border-gray-200 bg-gray-50 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black transition"
+          />
+        </div>
+        <div className="mb-6">
+          <a href="#" className="hover:underline font-medium text-[12px] pl-[280px] ">
+            Forgot Password?
+          </a>
+        </div>
+        <button className="w-full bg-black text-white py-3 rounded-[30px] font-semibold hover:bg-gray-900 transition">
+          Sign In
+        </button>
+       
+
+        <div className="flex items-center my-6">
+          <hr className="flex-1 border-gray-300" />
+          <span className="px-3 text-gray-500 text-sm">OR</span>
+          <hr className="flex-1 border-gray-300" />
+        </div>
+
+        <button
+          className="w-full border  py-3 rounded-[30px] flex items-center justify-center gap-3 text-black font-medium "
+          aria-label="Sign in with Google"
+        >
+          <img src="/images/google.png" className="max-w-5" alt="" />
+          Continue with Google
+        </button>
+        <div>
+            <p className="mt-6 text-center text-gray-500 text-sm">
+                Don't have an account?{" "}
+                <a href="#" className="hover:underline font-medium text-black">
+                Sign Up
+                </a>
+            </p>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default SignInCard;
---
+++ news_brief/src/components/signup_components/signupCard.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const SignUpCard = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <div
+        className="w-full max-w-md  rounded-2xl shadow-xl p-8"
+        style={{ background: "rgb(245, 245, 245)" }}
+      >
+        <h1 className="text-3xl font-bold text-center text-gray-900 mb-6">
+          Create your account
+        </h1>
+        <div className="mb-2">
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="w-full px-4 py-3 rounded-xl border border-gray-200 bg-gray-50 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black transition"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="email"
+            placeholder="Email"
+            className="w-full px-4 py-3 rounded-xl border border-gray-200 bg-gray-50 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black transition"
+          />
+        </div>
+
+        <div className="mb-2">
+          <input
+            type="password"
+            placeholder="Password"
+            className="w-full px-4 py-3 rounded-xl border border-gray-200 bg-gray-50 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black transition"
+          />
+        </div>
+
+        <div className="mb-6">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            className="w-full px-4 py-3 rounded-xl border border-gray-200 bg-gray-50 text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black transition"
+          />
+        </div>
+        <button className="w-full bg-black text-white py-3 rounded-[30px] font-semibold hover:bg-gray-900 transition">
+          Sign Up
+        </button>
+
+        <div className="flex items-center my-6">
+          <hr className="flex-1 border-gray-300" />
+          <span className="px-3 text-gray-500 text-sm">OR</span>
+          <hr className="flex-1 border-gray-300" />
+        </div>
+
+        <button
+          className="w-full border  py-3 rounded-[30px] flex items-center justify-center gap-3 text-black font-medium "
+          aria-label="Sign in with Google"
+        >
+          <img src="/images/google.png" className="max-w-5" alt="" />
+          Continue with Google
+        </button>
+        <div>
+          <p className="mt-6 text-center text-gray-500 text-sm">
+            Already have an account?{" "}
+            <a href="#" className="hover:underline font-medium text-black">
+              Sign In
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpCard;
