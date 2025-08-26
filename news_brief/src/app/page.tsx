bf1c651e2ef0d11aba54c1630584a6a91562adc4
meklit <meklitmelkamu34@gmail.com>
2025-08-26 21:01:50 +0300

    added notification component

--- news_brief/src/app/page.tsx
+++ news_brief/src/app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
+import Notification from "../components/notification_component/notification";
 
 export default function Home() {
-  const router = useRouter();
+  return(
+    <Notification/>
+  )
+  // const router = useRouter();
 
-  useEffect(() => {
-    router.replace("/news");
-  }, [router]);
-  return <></>;
+  // useEffect(() => {
+  //   router.replace("/news");
+  // }, [router]);
+  // return <></>;
 }
