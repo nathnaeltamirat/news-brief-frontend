5b74ef7843be9720184b72f129f82948a128ff0d
meklit <meklitmelkamu34@gmail.com>
2025-08-27 11:50:35 +0300

    added topic header to the notification settings

--- news_brief/src/app/profile/notification/page.tsx
+++ news_brief/src/app/profile/notification/page.tsx
@@ -1,18 +1,18 @@
-import Notification from '@/components/notification_component/notification'
-import Sidebar from '@/components/siderbar/profile'
-import React from 'react'
+import Notification from "@/components/notification_component/notification";
+import Sidebar from "@/components/siderbar/profile";
+import React from "react";
 
 const page = () => {
   return (
     <div className="flex bg-gray-50 min-h-screen">
-      <div>
-        <Sidebar />
-      </div>
-      <div className='p-8 flex-1'>
+      <Sidebar />
+
+      <div className="flex-1 p-8 flex flex-col">
+        <div className="text-2xl font-bold mb-6">Notification Settings</div>
         <Notification />
       </div>
     </div>
   );
-}
+};
 
-export default page
+export default page;
