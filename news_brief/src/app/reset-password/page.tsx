464257f5d4301a658deb42eefe33ae27243831d6
nebutty <nebathussen2@gmail.com>
2025-09-01 15:56:37 +0300

    Reset password Sucesffull Build Succesfull Delete branch

--- news_brief/src/app/reset-password/page.tsx
+++ news_brief/src/app/reset-password/page.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import ResetPasswordForm from "../../components/reset_password/reset_pass";
+import { Suspense } from "react";
 
 export default function ResetPasswordPage() {
-  return(
-  <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <ResetPasswordForm />
-    </div>)
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <Suspense fallback={<div>Loading reset form...</div>}>
+        <ResetPasswordForm />
+      </Suspense>
+    </div>
+  );
 }
