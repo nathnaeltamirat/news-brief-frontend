e3268badb3eed076e4b29a7c545fd589247dc524
nathnaeltamirat <nathnaeltamirat3@gmail.com>
2025-09-03 02:05:29 -0900

    ADDing The ICON

--- news_brief/src/app/news/page.tsx
+++ news_brief/src/app/news/page.tsx
@@ -31,7 +31,7 @@ const News = () => {
             <TopBar />
           </div>
           <NewsComponent />
-          <ProfileDropdown/>
+     
           {/* <Footer /> */}
         </div>
       </div>
